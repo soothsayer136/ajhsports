a8b6c3bd281e623c11993f05af6652c021deecae
soothsayer136 <bikrum.katwal@gmail.com>
2024-05-14 18:57:00 +0545

    herosection ui chnages

--- src/components/HomeComponents/GetStarted.js
+++ src/components/HomeComponents/GetStarted.js
@@ -6,8 +6,8 @@ function GetStarted() {
         <div className='bg-secondary p-5 py-28'>
             <div className='grid md:grid-cols-2 max-w-7xl mx-auto items-center justify-between gap-10'>
                 <div className='flex flex-col gap-4 lg:px-20 px-5'>
-                    <h3 className='text-4xl font-semibold'>Getting started with Albino is easier than ever</h3>
-                    <h3 className=''>With lots of unique blocks, you can easily build a page without coding. Build your next landing page so quickly with Albino.</h3>
+                    <h3 className='text-4xl font-semibold'>Social Tennis and Table Tennis</h3>
+                    <h3 className=''>AJH Sports provides social and competitive tennis sessions for players of all skill levels. We charge a $5 entry for two hours of hitting, great for fiend groups who need a couple of extra players to form a round robin.</h3>
                     <button className='btn-primary max-w-fit'>Get started for free <FaArrowRight /></button>
                 </div>
                 <div className='p-5'>
--- src/components/HomeComponents/HeroSection.js
+++ src/components/HomeComponents/HeroSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function HeroSection() {
     return (
@@ -7,7 +8,7 @@ function HeroSection() {
             <div className='flex flex-col justify-center justify-items-center items-center  gap-4 h-72'>
                 <h1 className='lg:text-6xl text-4xl font-bold'>Tennis Lessons</h1>
                 <h2 className=''>Professional private, semi-private and group tennis lessons</h2>
-                <button className='bg-current p-3 px-6 rounded text-white mt-4 flex gap-6 items-center'>Ask Andrew <FaArrowRight /> </button>
+                <Link to={'/contact'} className='bg-current p-3 px-6 rounded text-white mt-4 flex gap-6 items-center'>Ask Andrew <FaArrowRight /> </Link>
             </div>
 
             <div>
