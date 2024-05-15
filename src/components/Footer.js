a3be6fde21bf55940cc1a466ad411f215c8ff728
soothsayer136 <bikrum.katwal@gmail.com>
2024-05-15 22:05:08 +0545

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,59 @@
 import React from 'react'
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter, BsYoutube } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
         <footer className="bg-white rounded-lg shadow  ">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="/app_logo.png" className="h-8" alt="AJH SPORTS" />
-                        {/* <span className="self-center text-2xl font-semibold whitespace-nowrap ">AJH Sports</span> */}
-                    </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+                <div className="grid grid-cols-6">
+                    <ul className='sm:col-span-4 col-span-full sm:mb-0 mb-10'>
+                        <a href="/" className="grid items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                            <img src="/app_logo.png" className="h-8" alt="AJH SPORTS" />
+                        </a>
+                        <p className='mt-5 text-gray-600'> We are here to help the customers <br /> to get their success</p>
+                        <div className='flex gap-3 mt-5'>
+                            <Link to={'/https://www.instagram.com/starstv/'} target='blank'>
+                                <BsInstagram />
+                            </Link>
+                            <Link to={'/https://www.facebook.com/aussiestarstv/'} target='blank'>
+                                <BsFacebook />
+                            </Link>
+                            <Link to={'https://www.youtube.com/user/clubSTARStv'} target='blank'>
+                                <BsYoutube />
+                            </Link>
+                            <Link to={'https://twitter.com/starstv'} target='blank'>
+                                <BsTwitter />
+                            </Link>
+                            <Link to={'https://www.linkedin.com/in/starstv/'} target='blank'>
+                                <BsLinkedin />
+                            </Link>
+                        </div>
+                    </ul>
+
+                    <ul className="grid sm:col-span-1 col-span-3 items-center gap-3 mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+                        <li className='mb-5'>
+                            <label className=" me-4 md:me-6">Services</label>
+                        </li>
                         <li>
                             <a href="/about" className="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="/events" className="hover:underline">Events</a>
+                        </li>
+                        <li>
+                            <a href="/coaching" className="hover:underline">Bookings</a>
+                        </li>
+                    </ul>
+                    <ul className="grid sm:col-span-1 col-span-3 items-center gap-3 mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+                        <li className='mb-5'>
+                            <label className=" me-4 md:me-6">Quick Links</label>
+                        </li>
+                        <li>
+                            <a href="/forum" className="hover:underline me-4 md:me-6">Forum</a>
+                        </li>
+                        <li>
+                            <a href="/blogs" className="hover:underline me-4 md:me-6">Blogs</a>
                         </li>
                         <li>
                             <a href="/contact" className="hover:underline">Contact</a>
